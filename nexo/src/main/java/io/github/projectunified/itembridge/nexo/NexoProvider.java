b798822c1a18881a4c464ca9d4414f7db2fc28a6
hsgamer <huynhqtienvtag@gmail.com>
2025-05-03 09:50:40 +0700

    init nexo

---
+++ nexo/src/main/java/io/github/projectunified/itembridge/nexo/NexoProvider.java
@@ -0,0 +1,33 @@
+package io.github.projectunified.itembridge.nexo;
+
+import com.nexomc.nexo.api.NexoItems;
+import com.nexomc.nexo.items.ItemBuilder;
+import io.github.projectunified.itembridge.api.ItemProvider;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class NexoProvider implements ItemProvider {
+    public static boolean isAvailable() {
+        return Bukkit.getPluginManager().getPlugin("Nexo") != null;
+    }
+
+    @Override
+    public @NotNull String[] type() {
+        return new String[]{
+                "nexo"
+        };
+    }
+
+    @Override
+    public @Nullable String id(@NotNull ItemStack item) {
+        return NexoItems.idFromItem(item);
+    }
+
+    @Override
+    public @Nullable ItemStack item(@NotNull String id) {
+        ItemBuilder builder = NexoItems.itemFromId(id);
+        return builder != null ? builder.build() : null;
+    }
+}
