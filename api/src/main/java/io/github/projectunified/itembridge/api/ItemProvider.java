3936276c49352d84ddb0d812b2c380ce07f9e480
hsgamer <huynhqtienvtag@gmail.com>
2025-05-03 09:03:36 +0700

    init api

---
+++ api/src/main/java/io/github/projectunified/itembridge/api/ItemProvider.java
@@ -0,0 +1,19 @@
+package io.github.projectunified.itembridge.api;
+
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public interface ItemProvider {
+    @NotNull String[] type();
+
+    @Nullable String id(@NotNull ItemStack item);
+
+    @NotNull ItemStack item(@NotNull String id);
+
+    default boolean isSimilar(@NotNull ItemStack item, @NotNull String id) {
+        return Objects.equals(id, id(item));
+    }
+}
