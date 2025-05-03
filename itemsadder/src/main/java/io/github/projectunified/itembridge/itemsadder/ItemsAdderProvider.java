ca7ac0c16b18e377b36681e86d243c40141289f3
hsgamer <huynhqtienvtag@gmail.com>
2025-05-03 09:40:59 +0700

    init itemsadder

---
+++ itemsadder/src/main/java/io/github/projectunified/itembridge/itemsadder/ItemsAdderProvider.java
@@ -0,0 +1,34 @@
+package io.github.projectunified.itembridge.itemsadder;
+
+import dev.lone.itemsadder.api.CustomStack;
+import io.github.projectunified.itembridge.api.ItemProvider;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemsAdderProvider implements ItemProvider {
+    public static boolean isAvailable() {
+        return Bukkit.getPluginManager().getPlugin("ItemsAdder") != null;
+    }
+
+    @Override
+    public @NotNull String[] type() {
+        return new String[]{
+                "itemsadder",
+                "ia"
+        };
+    }
+
+    @Override
+    public @Nullable String id(@NotNull ItemStack item) {
+        CustomStack customStack = CustomStack.byItemStack(item);
+        return customStack != null ? customStack.getNamespacedID() : null;
+    }
+
+    @Override
+    public @Nullable ItemStack item(@NotNull String id) {
+        CustomStack customStack = CustomStack.getInstance(id);
+        return customStack != null ? customStack.getItemStack() : null;
+    }
+}
