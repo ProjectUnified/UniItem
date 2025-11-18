7a6bc2a3754e0da41eabdba6146e4fc015df9820
hsgamer <huynhqtienvtag@gmail.com>
2025-11-18 20:08:48 +0700

    move all methods on CraftEngine to a util class

--- craftengine/src/main/java/io/github/projectunified/uniitem/craftengine/CraftEngineItemProvider.java
+++ craftengine/src/main/java/io/github/projectunified/uniitem/craftengine/CraftEngineItemProvider.java
@@ -1,10 +1,6 @@
 package io.github.projectunified.uniitem.craftengine;
 
 import io.github.projectunified.uniitem.api.SimpleItemProvider;
-import net.momirealms.craftengine.bukkit.api.BukkitAdaptors;
-import net.momirealms.craftengine.bukkit.api.CraftEngineItems;
-import net.momirealms.craftengine.core.item.CustomItem;
-import net.momirealms.craftengine.core.util.Key;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -12,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class CraftEngineItemProvider implements SimpleItemProvider {
+    private final CraftEngineUtil craftEngineUtil = new CraftEngineUtil();
+
     public static boolean isAvailable() {
         return Bukkit.getPluginManager().getPlugin("CraftEngine") != null;
     }
@@ -23,21 +21,16 @@ public class CraftEngineItemProvider implements SimpleItemProvider {
 
     @Override
     public @Nullable String id(@NotNull ItemStack item) {
-        Key key = CraftEngineItems.getCustomItemId(item);
-        return key != null ? key.asString() : null;
+        return craftEngineUtil.id(item);
     }
 
     @Override
     public @Nullable ItemStack item(@NotNull String id) {
-        Key key = Key.of(id);
-        CustomItem<ItemStack> customItem = CraftEngineItems.byId(key);
-        return customItem != null ? customItem.buildItemStack() : null;
+        return craftEngineUtil.item(id);
     }
 
     @Override
     public @Nullable ItemStack item(@NotNull String id, @NotNull Player player) {
-        Key key = Key.of(id);
-        CustomItem<ItemStack> customItem = CraftEngineItems.byId(key);
-        return customItem != null ? customItem.buildItemStack(BukkitAdaptors.adapt(player)) : null;
+        return craftEngineUtil.item(id, player);
     }
 }
